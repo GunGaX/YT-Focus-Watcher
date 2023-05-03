63c242efd57f7ea569f3968f902b3ff15febdeee
GunGaX <118834936+GunGaX@users.noreply.github.com>
2023-05-03 23:47:17 +0300

    little model changes

--- Focus Watcher/Model.swift
+++ Focus Watcher/Model.swift
@@ -11,12 +11,27 @@ class Model {
     func getVideos() {
         
         // Create a URL object
+        let url = URL(string: Constants.API_URL)
+        
+        guard url != nil else {
+            return
+        }
         
         // Get a URLSession object
+        let session = URLSession.shared
         
         // Get a data task from session
+        let dataTask = session.dataTask(with: url!) { (data, response, error) in
+            
+            if error != nil || data == nil {
+                return
+            }
+            
+            // Parsing the data
+            
+        }
         
         // Kick off the task
-        
+        dataTask.resume()
     }
 }
